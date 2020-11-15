37eee82fd3e0463408f4b7a2e0bb5431f8086ed4
poet038 <53939264+poet038@users.noreply.github.com>
2020-11-15 21:32:45 +0100

    work

--- helpers/listFileContent.py
+++ helpers/listFileContent.py
@@ -1,14 +1,69 @@
-import sys 
+import sys
+import re as pattern
+from termcolor import colored
 
-def create_command_and_alias_groups(alias_block):
+groupNames = []
+
+
+def slice(text, char, occur):
+    string = ""
+    counter = 0
+    for c in range(len(text)):
+        if counter != occur:
+            string += text[c]
+            if text[c] == char:
+                counter = counter + 1
+    return string.strip()
+
+
+def get_command(string):
+    slicer = string.replace(slice(string, "'", 1), "")
+    return slicer.split(" ")[0]
+
+
+def is_command(commandToCheck):
+    from shutil import which
+    if len(commandToCheck.split(" ")) == 1:
+        return which(commandToCheck) is not None
+    else:
+        return which(get_command(commandToCheck)) is not None
+
+
+def create_command_and_alias_groups(text_content):
     ALIAS = "alias"
-    if alias_block.startswith(ALIAS):
-        print(alias_block)
+    print(text_content)
+
+
+def file_check(text_content):
+    groupPattern = pattern.compile("\[(.*?)\]")
+    aliasPattern = pattern.compile("alias (.*?)\S='\S(.*?)'")
+    aliasList = []
+    for line in text_content.split('\n'):
+        if aliasPattern.match(line) or groupPattern.match(line):
+            if aliasPattern.match(line):
+                slicedLine = slice(line.strip(), "\'", 2)
+                command = get_command(slicedLine)
+                if is_command(slicedLine):
+                    if slicedLine not in aliasList:
+                        aliasList.append(slicedLine)
+                    if command not in groupNames:
+                        groupNames.append(command)
+                    print(aliasList)
+                else:
+                    print(colored("command: {} not found on local-system".format(command), 'red'))
+        else:
+            print(colored("Removing line: *{}* due to wrong pattern".format(line), 'red'))
+
+    print(groupNames)
+
+
 def main():
-    print("--------------------\nin python:")
-    print(sys.argv[1])
-    print(sys.argv[2])
-    with open("/home/carlo/.bash_aliases", "r") as aliasrc:
-        for line in aliasrc:
-            create_command_and_alias_groups(line)
-main() 
+    # print(sys.argv[1])
+    # print(sys.argv[2])
+    with open("../test/.bash_aliases", "r") as aliasrc:
+        file_check(aliasrc.read())
+    # create_command_and_alias_groups(aliasrc.read())
+    # file operations come here
+
+
+main()
---
+++ test/.bash_aliases
@@ -0,0 +1,6 @@
+[kubectl]
+alias la='ls -la'
+alias kbc='kubectl get pods' hhh
+alias happy='node get pods' hhh
+alias happy='npm --version' hhh
+alias ksks=''
