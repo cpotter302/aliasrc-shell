3b787c4ef585c950f3371be5c2952128eedbe5dd
Carlo Pötter <carlo.poetter@bertelsmann.de>
2020-11-16 09:45:05 +0100

    ternary and enumeration loop

--- src/main/pyhton/scripts/listFileContent.py
+++ src/main/pyhton/scripts/listFileContent.py
@@ -4,6 +4,7 @@ from termcolor import colored
 
 groupNames = []
 
+get_command = lambda string: string.replace(slice(string, "'", 1), "").split(" ")[0]
 
 def slice(text, char, occur):
     string = ""
@@ -14,11 +15,6 @@ def slice(text, char, occur):
             if text[c] == char:
                 counter = counter + 1
     return string.strip()
-
-
-def get_command(string):
-    slicer = string.replace(slice(string, "'", 1), "")
-    return slicer.split(" ")[0]
 
 
 def is_command(commandToCheck):
@@ -33,8 +29,8 @@ def file_check(text_content):
     groupPattern = pattern.compile("\[(.*?)\]")
     aliasPattern = pattern.compile("alias (.*?)\S='\S(.*?)'")
     aliasList = []
-    fileArray = text_content.split('\n')
-    for line in fileArray:
+
+    for index, line in enumerate(text_content.split('\n'), start=1):
         if aliasPattern.match(line) or groupPattern.match(line):
             if aliasPattern.match(line):
                 slicedLine = slice(line.strip(), "\'", 2)
@@ -47,9 +43,9 @@ def file_check(text_content):
                     if command not in groupNames:
                         groupNames.append(command)
                 else:
-                    print(colored("command: {} not found on local-system".format(command), 'red'))
-        else:
-            print(colored("Removing line {}: {}  due to wrong pattern".format(fileArray.index(line) + 1, line), 'red'))
+                    print(colored("command '{}' not found on local-system".format(command), 'red'))
+        elif line.strip():
+            print(colored("Removing line {}: {}  due to wrong pattern".format(index, line), 'red'))
 
     return aliasList
 
@@ -59,6 +55,7 @@ def main():
     # print(sys.argv[2])
     print("🔎   checking alias-file")
     mainList = []
+    
     with open(".bash_aliases.back", "r") as aliasrc:
         mainList = file_check(aliasrc.read())
     print("ℹ️   Check completed")
