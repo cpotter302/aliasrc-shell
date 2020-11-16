0b3747b6f85483ab1655bd973b9eebef71f7ebb2
Carlo Pötter <carlo.poetter@bertelsmann.de>
2020-11-16 14:46:25 +0100

    changed arg types

--- shell/alias.sh
+++ shell/alias.sh
@@ -11,7 +11,7 @@ do
    case "$opt" in
       a ) alias="$OPTARG" ;; #alias
       c ) command="$OPTARG" ;; # command
-      g ) group=true ;; # print group with -g -c kubectl
+      g ) group="$OPTARG";; # print group with -g -c kubectl
       p ) print=true ;; # print
       d ) delete=true ;; # delete file content
       e ) editor=$OPTARG ;;# edit .bash_aliasrc with given editor
@@ -22,26 +22,24 @@ shift $((OPTIND -1))
 
 export ALIAS_RC_ROOT=~/.bash_aliasrc
 #Check parameters
-if [ "$alias" ] && [ "$command" ]
-then
-echo "call insert method"
-elif [ "$group" == true ] && [ -z "$command" ]
-then
-echo "print commandgroup"
-elif [ "$print" == true ]
-then
-echo "print bash_aliases"
-elif [ "$delete" == true ] && [ $( whoami ) == "root" ]
-then 
-functions/pruneAliasFile.sh
-elif [ "$delete" == true ] && [ ! $( whoami ) == "root" ] 
-then
-echo "run command as sudo user" && exit 1
-elif [ "$editor" ]
-then
-functions/editAliases.sh $editor
+if [ "$alias" ] && [ "$command" ]; then
+   echo "call insert method"
+elif [ "$group" ] && [ "$print" == true ]; then
+   echo "print commandgroup"
+elif [ "$print" == true ]; then
+   echo "print bash_aliases"
+elif [ "$delete" == true ] && [ "$alias" ]; then
+   echo "delete given alias: $alias"
+elif [ "$delete" == true ] && [ "$group" ]; then 
+   echo "Deleting given group: $group"
+elif [ "$delete" == true ] && [ $( whoami ) == "root" ]; then 
+   functions/pruneAliasFile.sh
+elif [ "$delete" == true ] && [ ! $( whoami ) == "root" ]; then
+   echo "run command as sudo user" && exit 1
+elif [ "$editor" ]; then
+   functions/editAliases.sh $editor
 else 
-helpFunction
+   helpFunction
 fi
 
 #python3 ../python/scripts/listFileContent.py "$alias" "$command"
