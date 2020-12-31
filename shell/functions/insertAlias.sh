e8266c90a4a0965f2ba0197c5723458bdb9b5fe9
poet038 <53939264+poet038@users.noreply.github.com>
2020-12-31 13:59:45 +0100

    modiefid script

--- shell/functions/insertAlias.sh
+++ shell/functions/insertAlias.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f "$ALIAS_RC_ROOT" ]; then
-  if ! grep -q "$1" <"$ALIAS_RC_ROOT" && ! command -v "$1" &>/dev/null; then
+  if ! grep -q "$1" <"$ALIAS_RC_ROOT"; then
     echo "alias $1='$2'" >>"$ALIAS_RC_ROOT"
-  elif grep -q "$1" <"$ALIAS_RC_ROOT" && ! command -v "$1" &>/dev/null && [ -n "$3" ]; then
+  elif grep -q "$1" <"$ALIAS_RC_ROOT" && [ -n "$3" ]; then
     functions/deleteAlias.sh "$1"
     echo "alias $1='$2'" >>"$ALIAS_RC_ROOT"
   else
@@ -13,3 +13,5 @@ if [ -f "$ALIAS_RC_ROOT" ]; then
 else
   echo "File named $ALIAS_RC_ROOT does not exist"
 fi
+
+#&& ! command -v "$1" &>/dev/null
