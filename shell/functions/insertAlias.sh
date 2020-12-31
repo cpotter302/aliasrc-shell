1b31f2fdf4a2953ac7597ce3cb156bcaad2e79a9
poet038 <53939264+poet038@users.noreply.github.com>
2020-12-31 15:08:32 +0100

    modiefid script

--- shell/functions/insertAlias.sh
+++ shell/functions/insertAlias.sh
@@ -4,11 +4,9 @@ if [ -f "$ALIAS_RC_ROOT" ]; then
   if grep -q "$1" <"$ALIAS_RC_ROOT" && [ -n "$3" ]; then
     functions/deleteAlias.sh "$1"
     echo "alias $1='$2'" >>"$ALIAS_RC_ROOT"
-    echo "first"
     exit 1
   elif [ -n "$1" ] && [ "$(functions/printBashAliases.sh | grep "$1"| cut -d'=' -f 1 | awk '{print $2}')" != "$1" ]; then
     echo "alias $1='$2'" >>"$ALIAS_RC_ROOT"
-    echo "fs"
     exit 1
   else
     echo "Encountered problems when trying to add alias '$1'"
