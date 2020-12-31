77e97aa77142f1dc87b4c304be6a1a96fc1fcc07
poet038 <53939264+poet038@users.noreply.github.com>
2020-12-31 15:26:35 +0100

    changed file loc

--- shell/functions/insertAlias.sh
+++ shell/functions/insertAlias.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f "$ALIAS_RC_ROOT" ]; then
-  if ! grep -q "$1" <"$ALIAS_RC_ROOT"; then
+  if ! grep -wq "$1" <"$ALIAS_RC_ROOT"; then
     echo "alias $1='$2'" >>"$ALIAS_RC_ROOT"
-  elif grep -q "$1" <"$ALIAS_RC_ROOT" && [ -n "$3" ]; then
+  elif grep -wq "$1" <"$ALIAS_RC_ROOT" && [ -n "$3" ]; then
     functions/deleteAlias.sh "$1"
     echo "alias $1='$2'" >>"$ALIAS_RC_ROOT"
   else
